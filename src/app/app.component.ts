67c0886b7b49115d81de9330462e6794f14e92e8
Wilson Andres Piravaguen <wapiravaguens@unal.edu.co>
2018-04-12 23:05:50 -0500

    Tutorial Completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    //template: `<h1>{{ title }}</h1>` (in line)
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {ImageService} from './services/image.service';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageListComponent } from './gallery/image-list/image-list.component';
+import { ImageComponent } from './gallery/image-list/image.component';
+import { ImageDetailComponent } from './gallery/image-detail/image-detail.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
 
+
+import {Routes, RouterModule} from '@angular/router';
+
+
+const appRoutes: Routes = [
+    { path: '', redirectTo:'/gallery', pathMatch: 'full'},
+    { path: 'gallery', component: GalleryComponent},
+    { path: 'contact', component: ContactComponent},
+    { path: 'about', component: AboutComponent} 
+    ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GalleryComponent,
+    ImageListComponent,
+    ImageComponent,
+    ImageDetailComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
-  providers: [],
+  providers: [ImageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
